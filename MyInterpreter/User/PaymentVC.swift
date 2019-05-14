d501205bf95c2d22b8d0087a9df444ad360f9b1e
Tom Cao <itom41098@gmail.com>
2019-05-14 19:00:24 +0700

    Payment view updated

--- MyInterpreter/User/PaymentVC.swift
+++ MyInterpreter/User/PaymentVC.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class PaymentVC: UIViewController {
 
+
+    @IBOutlet weak var pickerPaymentMethod: UIPickerView!
+    @IBOutlet weak var btnAboutPayment: UIButton!
+    @IBOutlet weak var txtFieldNumberOfDays: UITextField!
+    @IBOutlet weak var lblPrice: UILabel!
+    
+    @IBOutlet weak var txtFieldCardName: UITextField!
+    @IBOutlet weak var txtFieldCardNumber: UITextField!
+    @IBOutlet weak var txtFieldExpDate: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
